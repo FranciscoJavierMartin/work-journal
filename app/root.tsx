780b3ff74cac266c1ec069463f82ed364a355e94
Francisco <javiermartincuenca@gmail.com>
2023-10-17 23:47:10 +0200

    Setup tailwind prettier

--- app/root.tsx
+++ app/root.tsx
@@ -16,15 +16,15 @@ export const links: LinksFunction = () => [
         { rel: 'stylesheet', href: cssBundleHref },
         { rel: 'stylesheet', href: stylesheet },
       ]
-    : []),
+    : [{ rel: 'stylesheet', href: stylesheet }]),
 ];
 
 export default function App() {
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
-        <meta charSet='utf-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,33 +9,8 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-      <ul>
-        <li>
-          <a
-            target='_blank'
-            href='https://remix.run/tutorials/blog'
-            rel='noreferrer'
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target='_blank'
-            href='https://remix.run/tutorials/jokes'
-            rel='noreferrer'
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="bg-gray-300 p-20">
+      <p className="text-blue-500">Hello, Build UI!</p>
     </div>
   );
 }
