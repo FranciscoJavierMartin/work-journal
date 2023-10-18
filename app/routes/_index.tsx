024cfe0a8da93119faee33bb389b361d71ebe259
Francisco <javiermartincuenca@gmail.com>
2023-10-18 22:06:10 +0200

    Add form

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,50 +1,118 @@
-import type { MetaFunction } from '@remix-run/node';
+import {
+  redirect,
+  type MetaFunction,
+  type ActionFunctionArgs,
+} from '@remix-run/node';
+import { Form } from '@remix-run/react';
+
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const json = Object.fromEntries(formData);
+
+  console.log('Action', json);
+
+  return redirect('/');
+}
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: 'New Remix App' },
-    { name: 'description', content: 'Welcome to Remix!' },
-  ];
+  return [{ title: 'Work journal' }];
 };
 
 export default function Index() {
   return (
-    <div className="max-w-7xl p-6 mx-auto ">
-      <h1 className="text-4xl text-white">Work journal</h1>
-      <p className="mt-3 text-xl text-gray-400">
+    <div className='max-w-7xl p-6 mx-auto '>
+      <h1 className='text-4xl text-white'>Work journal</h1>
+      <p className='mt-3 text-xl text-gray-400'>
         Doings and learnings. Updated weekly.
       </p>
 
-      <div className="mt-8">
-        <ul>
-          <li>
-            <p>
-              Week of Feb 2<sup>nd</sup>, 2023
-            </p>
+      <div className='my-8 border p-3'>
+        <Form method='post'>
+          <p className='italic'>Create an entry</p>
 
-            <div className="mt-4 space-y-4">
-              <div>
-                <p>Work:</p>
-                <ul className="ml-6 list-disc">
-                  <li>First thing</li>
-                </ul>
-              </div>
-              <div>
-                <p>Learnings:</p>
-                <ul className="ml-6 list-disc">
-                  <li>First learning</li>
-                  <li>Second learning</li>
-                </ul>
-              </div>
-              <div>
-                <p>Interesting things:</p>
-                <ul className="ml-6 list-disc">
-                  <li>Something cool!</li>
-                </ul>
-              </div>
+          <div>
+            <div className='mt-4'>
+              <input type='date' name='date' className='text-gray-700' />
             </div>
-          </li>
-        </ul>
+            <div className='mt-2 space-x-6'>
+              <label htmlFor='work'>
+                <input
+                  className='mr-1'
+                  id='work'
+                  type='radio'
+                  name='category'
+                  value='work'
+                />
+                Work
+              </label>
+              <label htmlFor='learning'>
+                <input
+                  className='mr-1'
+                  id='learning'
+                  type='radio'
+                  name='category'
+                  value='learning'
+                />
+                Learning
+              </label>
+              <label htmlFor='interesting-thing'>
+                <input
+                  className='mr-1'
+                  id='interesting-thing'
+                  type='radio'
+                  name='category'
+                  value='interesting-thing'
+                />
+                Interesting thing
+              </label>
+            </div>
+            <div className='mt-2'>
+              <textarea
+                name='text'
+                className='w-full text-gray-700'
+                placeholder='Write your entry...'
+              />
+            </div>
+            <div className='mt-1 text-right'>
+              <button
+                className='bg-blue-500 text-white font-medium px-4 py-1'
+                type='submit'
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </Form>
+      </div>
+
+      <div className='mt-6'>
+        <p className='font-bold'>
+          Week of February 20<sup>th</sup>
+        </p>
+
+        <div className='mt-3 space-y-4'>
+          <div>
+            <p>Work</p>
+            <ul className='ml-8 list-disc'>
+              <li>First item</li>
+              <li>Second item</li>
+            </ul>
+          </div>
+          <div>
+            <p>Learnings</p>
+            <ul className='ml-8 list-disc'>
+              <li>First item</li>
+              <li>Second item</li>
+            </ul>
+          </div>
+          <div>
+            <p>Interesting things</p>
+            <ul className='ml-8 list-disc'>
+              <li>First item</li>
+              <li>Second item</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
