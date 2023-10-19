8d781ffb6a810d1aa4fef2d0e5ab1fa5ef048e9a
Francisco <javiermartincuenca@gmail.com>
2023-10-19 23:11:37 +0200

    Display entries

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,7 @@
 import { type MetaFunction, type ActionFunctionArgs } from '@remix-run/node';
 import { useFetcher, useLoaderData } from '@remix-run/react';
 import { PrismaClient } from '@prisma/client';
-import { format } from 'date-fns';
+import { format, parseISO, startOfWeek } from 'date-fns';
 import { useEffect, useRef } from 'react';
 
 export async function action({ request }: ActionFunctionArgs) {
@@ -30,7 +30,35 @@ export async function loader() {
   const db = new PrismaClient();
   const entries = await db.entry.findMany();
 
-  return entries;
+  const entriesByWeek = entries
+    .map((entry) => ({
+      ...entry,
+      date: entry.date.toISOString().substring(0, 10),
+    }))
+    .reduce<Record<string, typeof entries>>((acc, entry) => {
+      const monday = startOfWeek(parseISO(entry.date), { weekStartsOn: 1 });
+      const mondayString = format(monday, 'yyyy-MM-dd');
+
+      acc[mondayString] ||= [];
+      acc[mondayString].push(entry as any);
+
+      return acc;
+    }, {});
+
+  const weeks = Object.keys(entriesByWeek)
+    .sort((a, b) => a.localeCompare(b))
+    .map((dateString) => ({
+      dateString,
+      work: entriesByWeek[dateString].filter((entry) => entry.type === 'work'),
+      learning: entriesByWeek[dateString].filter(
+        (entry) => entry.type === 'learning',
+      ),
+      interestingThings: entriesByWeek[dateString].filter(
+        (entry) => entry.type === 'interesting-thing',
+      ),
+    }));
+
+  return weeks;
 }
 
 export const meta: MetaFunction = () => {
@@ -40,14 +68,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   const fetcher = useFetcher();
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
-  const entries = useLoaderData<
-    {
-      id: number;
-      date: Date;
-      type: string;
-      text: string;
-    }[]
-  >();
+  const weeks = useLoaderData<typeof loader>();
 
   useEffect(() => {
     if (fetcher.state === 'idle' && textAreaRef.current) {
@@ -137,40 +158,47 @@ export default function Index() {
         </fetcher.Form>
       </div>
 
-      {entries.map((entry) => (
-        <p key={entry.id}>
-          {entry.type} - {entry.text}
-        </p>
-      ))}
-      {/* <div className='mt-6'>
-        <p className='font-bold'>
-          Week of February 20<sup>th</sup>
-        </p>
-
-        <div className='mt-3 space-y-4'>
-          <div>
-            <p>Work</p>
-            <ul className='ml-8 list-disc'>
-              <li>First item</li>
-              <li>Second item</li>
-            </ul>
+      <div className='mt-12 space-y-12'>
+        {weeks.map((week) => (
+          <div key={week.dateString}>
+            <p className='font-bold'>
+              Week of {format(parseISO(week.dateString), 'dd MMMM')}
+            </p>
+            <div className='mt-3 space-y-4'>
+              {week.work.length > 0 && (
+                <div>
+                  <p>Work</p>
+                  <ul className='ml-8 list-disc'>
+                    {week.work.map((entry) => (
+                      <li key={entry.id}>{entry.text}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              {week.learning.length > 0 && (
+                <div>
+                  <p>Learning</p>
+                  <ul className='ml-8 list-disc'>
+                    {week.learning.map((entry) => (
+                      <li key={entry.id}>{entry.text}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              {week.interestingThings.length > 0 && (
+                <div>
+                  <p>Interesting things</p>
+                  <ul className='ml-8 list-disc'>
+                    {week.interestingThings.map((entry) => (
+                      <li key={entry.id}>{entry.text}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </div>
           </div>
-          <div>
-            <p>Learnings</p>
-            <ul className='ml-8 list-disc'>
-              <li>First item</li>
-              <li>Second item</li>
-            </ul>
-          </div>
-          <div>
-            <p>Interesting things</p>
-            <ul className='ml-8 list-disc'>
-              <li>First item</li>
-              <li>Second item</li>
-            </ul>
-          </div>
-        </div>
-      </div> */}
+        ))}
+      </div>
     </div>
   );
 }
